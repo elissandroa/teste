d16d5e365e6e0ff22bcf28ec04fe1bbda86edcf6
Elissandro <elissandroa@hotmail.com>
2021-09-29 23:30:28 -0300

    Project created

---
+++ backend/src/test/java/com/elissandro/teste/TesteDevWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elissandro.teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDevWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
